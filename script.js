c0b10475ba99e304dc72032ad0ffcd4c3e37a53f
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-03-07 13:15:38 -0800

    Game complete with user prompt

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const rpsArray = ['Rock', 'Paper', 'Scissors'];
-const random = Math.floor(Math.random() * 3);
+// const random = Math.floor(Math.random() * 3);
 let playerWins = 0;
 let computerWins = 0;
 
+function random(){
+    return Math.floor(Math.random() * 3);
+}
+
 function computerPlay() {
-    return rpsArray[random];
+    return rpsArray[random()];
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -14,20 +18,20 @@ function playRound(playerSelection, computerSelection) {
 
     if((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper')) {
         playerWins++;
-        return `You win, ${player} beats ${computer}!`
+        return `You win, ${player} beats ${computer}! Score is currently - Player:${playerWins} Computer:${computerWins}`
     }
-    if((player===computer){
-        return `This round was a draw!`
+    if(player===computer){
+        return `This round was a draw, ${player} ties ${computer}! Score is currently - Player:${playerWins} Computer:${computerWins}`
     }
     computerWins++;
-    return `Computer wins, ${computer} beats ${player}`
+    return `Computer wins, ${computer} beats ${player}! Score is currently - Player:${playerWins} Computer:${computerWins}`
 }
+
+const playerChoice = prompt("What would you like to play? Please enter rock, paper, or scissors.");
 
 function game(i=0) {
     if(i === 5) return;
-    playRound('Rock', computerPlay())
+    console.log(playRound(playerChoice, computerPlay()));
     return game(i+1);
 }
-console.log(game)
-console.log(playerWins);
-console.log(computerWins);
+console.log(game())
